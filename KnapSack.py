7573f550d77918590e2a8df775c01123f15b2150
tejasclg <151636024+tejasclg@users.noreply.github.com>
2023-11-21 17:37:46 +0530

    Add files via upload

---
+++ KnapSack.py
@@ -0,0 +1,36 @@
+
+class Item:
+	def __init__(self, profit, weight):
+		self.profit = profit
+		self.weight = weight
+
+
+def fractionalKnapsack(W, arr):
+	
+	arr.sort(key=lambda x: (x.profit/x.weight), reverse=True)	
+	finalvalue = 0.0
+	
+	# Looping through all Items
+	for item in arr:
+		
+		if item.weight <= W:
+			W -= item.weight
+			finalvalue += item.profit
+		
+		else:
+			finalvalue += item.profit * W / item.weight
+			break
+	
+	
+	return finalvalue
+
+
+
+if __name__ == "__main__":
+	W = 50
+	arr = [Item(70, 10), Item(100, 20), Item(120, 30)]
+	max_val = fractionalKnapsack(W, arr)
+	print("Maximum value obtained is : ",max_val)
+	
+	
+
